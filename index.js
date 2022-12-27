c8bade06a6a3b6291ba382b5fed60488ae12ab4f
abilnf <slin.lbeog@gmail.com>
2022-12-27 23:36:07 +0100

    implement merge sort

--- index.js
+++ index.js
@@ -17,3 +17,17 @@ function fibsRec(n, arr = [0, 1]) {
 }
 
 console.log(fibsRec(8));
+
+function mergeSort(arr) {
+  if (arr.length < 2) return arr;
+  const left = mergeSort(arr.slice(0, arr.length / 2));
+  const right = mergeSort(arr.slice(arr.length / 2));
+  const out = [];
+  for (let i = 0; i < arr.length; i++)
+    out.push(
+      ((left[0] ?? Infinity) < (right[0] ?? Infinity) ? left : right).shift()
+    );
+  return out;
+}
+
+console.log(mergeSort([5, 8, 1, 3, 4, 6, 2, 7]));
